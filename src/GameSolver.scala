3bd056a37e485b2c1995ca96b7c432eaace07812
Vladimir Tsoy <tsoy_vladimir@yahoo.com>
2012-12-05 18:33:26 -0600

    initial setup

---
+++ src/GameSolver.scala
@@ -0,0 +1,17 @@
+/** Author:  M. Allen AND YOUR NAMES HERE
+  * 
+  * Solves the 8-puzzle game.
+  */
+  
+object GameSolver {
+
+  val defaultFile = "inputs/hw04_puzz1.txt"
+
+  def main( args: Array[String] ): Unit =
+    if ( !args.isEmpty ) {
+      val gm = new GameMaker( args( 0 ) )
+    }
+    else {
+      val gm = new GameMaker( defaultFile )
+    }
+}
