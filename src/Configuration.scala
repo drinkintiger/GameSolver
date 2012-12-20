b582e1e98d59ac10eae9510802bd360ec57b50da
drinkintiger <tsoy_vladimir@yahoo.com>
2012-12-20 17:30:36 -0600

    more work on Configuration

--- src/Configuration.scala
+++ src/Configuration.scala
@@ -1,7 +1,8 @@
  case class Configuration (config: List[Int]) {
-   val toMove = config.indexOf(0) 
+   val initialPath = config
+   val toMove = initialPath.indexOf(0) 
    
-   
+   type Configuration = List[Int]
    
    
    def move (m: Move): Configuration = {
@@ -11,19 +12,17 @@
        case Right => if (toMove == 2 || toMove == 5 || toMove == 8) false else true
        case Up => if (toMove <= 2) false else true
        case Down => if (toMove >= 6) false else true
+     }       
+       
+     def makeMove (m: Move): Configuration = m match {
+       case Left => val temp = initialPath(toMove - 1); initialPath updated ( toMove, temp ) updated ( config.indexOf(temp), 0 )
+       case Right => val temp = initialPath(toMove + 1); initialPath updated ( toMove, temp) updated ( config.indexOf(temp), 0)
+       case Up => val temp = initialPath(toMove - 3); initialPath updated ( toMove, temp) updated ( config.indexOf(temp), 0)
+       case Down => val temp = initialPath(toMove + 3); initialPath updated ( toMove, temp) updated ( config.indexOf(temp), 0)
      }
      
-     if (isValid(m)) 
-     
-     
-     Configuration(List(List(1)))
+     if (isValid(m)) makeMove(m) else config
    }
-   
-   
-   
-   
-   
-	
  }
 
  sealed abstract class Move
